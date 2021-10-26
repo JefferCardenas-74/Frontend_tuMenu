38f322b38727753f18a57e41688c5ed697247484
Jeffer Cardenas <jecgdevp@gmail.com>
2021-10-25 21:14:46 -0500

    se agrega el modulo de opciones

---
+++ src/components/Opcionales.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="contenedor-opcionales">
+    <div class="header">
+      <h2>{{ opcional }}</h2>
+
+      <div class="opciones-opcionales">
+
+        <button class="btn btn-danger" type="button">
+          <i class="fas fa-minus-circle"></i> ELIMINAR
+        </button>
+
+        <button class="btn btn-warning" type="button">
+          <i class="far fa-edit"></i> EDITAR
+        </button>
+      </div>
+    </div>
+
+    <br>
+
+    <div class="descripcion">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+        reprehenderit quia eaque! Debitis magni dignissimos quaerat autem,
+        quibusdam in nam laborum, modi maxime impedit ratione quis temporibus
+        fuga eum ducimus?
+      </p>
+    </div>
+
+    <div class="adicionales">
+      <div class="adicional">
+        <h5>Papas</h5>
+        <br>
+        <p>$ 3000</p>
+      </div>
+      <div class="adicional">
+        <h5>Gaseosa</h5>
+        <br>
+        <p>$ 2000</p>
+      </div>
+      <div class="adicional">
+        <h5>Salsa</h5>
+        <br>
+        <p>$ 1000</p>
+      </div>
+    </div>
+
+
+    <div class="opciones-adicionales">
+
+      <button class="btn btn-danger" type="button">
+        <i class="fas fa-minus-circle"></i> ELIMINAR
+      </button>
+
+      <button class="btn btn-warning" type="button">
+        <i class="far fa-edit"></i> EDITAR
+      </button>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ["opcional"],
+
+    data() {
+      return {};
+    },
+  };
+</script>
+
+<style scoped>
+  .contenedor-opcionales {
+    border: 1px solid;
+    padding: 25px;
+    border-radius: 20px;
+  }
+
+  .header {
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 1px solid;
+  }
+
+  .opciones-opcionales button {
+    margin-left: 15px;
+  }
+
+  .adicionales {
+    display: flex;
+    justify-content: flex-start;
+    flex-flow: row wrap;
+  }
+
+  .adicional {
+    margin: 10px;
+    padding: 15px;
+    border: 1px solid;
+    border-radius: 15px;
+  }
+  .opciones-adicionales{
+    display: flex;
+    justify-content: flex-start;
+  }
+
+  .opciones-adicionales button{
+    margin-left: 15px;
+  }
+
+</style>
--- src/views/pageCategorias.vue
+++ src/views/pageCategorias.vue
@@ -1,87 +1,172 @@
 <template>
-  <br>
+  <br />
   <div class="container">
-    <div class="container-opciones"> 
-      <button class="btn btn-success" type="button"> <i class="fas fa-plus"></i> AGREGAR CATEGORIA</button>
-      <button class="btn btn-success" type="button"> <i class="fas fa-plus"></i> AGREGAR PRODUCTO</button>
-      <button class="btn btn-success" type="button"> <i class="fas fa-plus"></i> AGREGAR LISTA DE OPCIONALES</button>
+    <div class="container-opciones">
+      <button class="btn btn-success" type="button">
+        <i class="fas fa-plus"></i> AGREGAR CATEGORIA
+      </button>
+      <button class="btn btn-success" type="button">
+        <i class="fas fa-plus"></i> AGREGAR PRODUCTO
+      </button>
+      <button class="btn btn-success" type="button">
+        <i class="fas fa-plus"></i> AGREGAR LISTA DE OPCIONALES
+      </button>
     </div>
-    
+
     <div class="container-titulo">
-      <Navbar></Navbar>
+      <Navbar @cambioSeccion="cambioSeccion()" :seleccion1="seleccion1" :seleccion2="seleccion2"></Navbar>
     </div>
 
-    <div class="row">
-      <div class="col">
-        <div class="lista-categorias">
-          <div class="list-group">
-            <a href="#" class="list-group-item list-group-item-action active" data-toggle="list" @click="toggleActive($event)" aria-current="true">
-              CATEGORIA 1
-            </a>
-            <a href="#" class="list-group-item list-group-item-action" data-toggle="list" @click="toggleActive($event)">CATEGORIA 2</a>
-            <a href="#" class="list-group-item list-group-item-action" data-toggle="list" @click="toggleActive($event)">CATEGORIA 3</a>
-            <a href="#" class="list-group-item list-group-item-action" data-toggle="list" @click="toggleActive($event)">CATEGORIA 4</a>
+    <div v-if="contenedorCategorias">
+      <div class="row">
+        <div class="col">
+          <div class="lista-categorias">
+            <div class="list-group">
+              <a href="#" class="list-group-item list-group-item-action active" data-toggle="list"
+                @click="toggleActive($event)" aria-current="true">
+                CATEGORIA 1
+              </a>
+              <a href="#" class="list-group-item list-group-item-action" data-toggle="list"
+                @click="toggleActive($event)">CATEGORIA 2</a>
+              <a href="#" class="list-group-item list-group-item-action" data-toggle="list"
+                @click="toggleActive($event)">CATEGORIA 3</a>
+              <a href="#" class="list-group-item list-group-item-action" data-toggle="list"
+                @click="toggleActive($event)">CATEGORIA 4</a>
+            </div>
           </div>
+        </div>
+        <div class="col-10 categorias">
+          <Categoria :nombre="nombre"></Categoria>
+        </div>
       </div>
-      </div>
-      <div class="col-10 categorias">
-        <Categoria :nombre="nombre"></Categoria>
-      </div>
+    </div>
 
+    <div v-if="contenedorOpcionales">
+      <div class="row">
+        <div class="col">
+          <div class="lista-categorias">
+            <div class="list-group">
+              <a href="#" class="list-group-item list-group-item-action active" data-toggle="list"
+                @click="toggleActiveOpcional($event)" aria-current="true">
+                OPCIONAL 1
+              </a>
+              <a href="#" class="list-group-item list-group-item-action" data-toggle="list"
+                @click="toggleActiveOpcional($event)">OPCIONAL 2</a>
+              <a href="#" class="list-group-item list-group-item-action" data-toggle="list"
+                @click="toggleActiveOpcional($event)">OPCIONAL 3</a>
+              <a href="#" class="list-group-item list-group-item-action" data-toggle="list"
+                @click="toggleActiveOpcional($event)">OPCIONAL 4</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-10">
+          <Opcionales :opcional="opcional"></Opcionales>
+        </div>
+      </div>
+    </div>
+
+    <br>
+    <br>
+
+    <div class="contenedor-url">
+      <div class="url">
+        <p>https://www.tumenu.com/pidea/mirestaurante/menu</p>
+      </div>
     </div>
   </div>
-  
 </template>
 
 <script>
-
-import Navbar from '../components/Navbar.vue';
-import Categoria from '../components/Categoria.vue';
+  import Navbar from "../components/Navbar.vue";
+  import Categoria from "../components/Categoria.vue";
+  import Opcionales from "../components/Opcionales.vue";
 
   export default {
     name: "App",
     components: {
       Navbar,
-      Categoria
+      Categoria,
+      Opcionales,
     },
     data() {
       return {
-        activo : true,
-        nombre : ''
+        activo: true,
+        nombre: "",
+        opcional: "",
+        contenedorCategorias: true,
+        contenedorOpcionales: false,
+        seleccion1: true,
+        seleccion2: false,
       };
     },
     methods: {
-
-      toggleActive(e){
+      toggleActive(e) {
         this.nombre = e.target.outerText;
-      }
+      },
 
+      toggleActiveOpcional(e) {
+        this.opcional = e.target.outerText;
+      },
+
+      cambioSeccion() {
+        if (this.contenedorCategorias && this.contenedorOpcionales == false) {
+
+          this.contenedorCategorias = false;
+          this.contenedorOpcionales = true;
+          this.seleccion1 = false;
+          this.seleccion2 = true;
+
+        } else if (
+
+          this.contenedorCategorias == false &&
+          this.contenedorOpcionales
+
+        ) {
+
+          this.contenedorCategorias = true;
+          this.contenedorOpcionales = false;
+          this.seleccion1 = true;
+          this.seleccion2 = false;
+
+        }
+      },
     },
   };
 </script>
 
 <style>
-
-  .container-opciones{
+  .container-opciones {
     display: flex;
     flex-flow: row wrap;
     justify-content: center;
   }
 
-  .container-opciones button{
+  .container-opciones button {
     margin: 15px;
   }
 
-  .container-categorias{
+  .container-categorias {
     display: flex;
   }
 
-  .categorias{
-
+  .categorias {
     border: 1px solid;
     padding: 10px;
     border-radius: 25px;
   }
 
+  .contenedor-url{
+    display: flex;
+    justify-content: center;
+    flex-flow: row wrap;
+  }
 
+  .url{
+    border: 1px solid;
+    padding: 15px;
+  }
+
+  .url p{
+    font-weight: bold;
+  }
 </style>
